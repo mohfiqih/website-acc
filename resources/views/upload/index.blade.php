787fc70f699a6d097f2c0b4dba7d1f408516778e
moh fiqih <mfiqiherinsyah90@gmail.com>
2024-09-25 10:17:07 +0700

    Update ACC v.1.1.4 (2)

--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -3,15 +3,14 @@
 
 <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload Image ACC</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
         rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
-    <link href="{{ asset('templates/assets/img/logo.jpg') }}" rel="icon">
+    <link href="https://amanahcitracemerlang.id/upload_image/1724591989_logojepang.png" rel="icon">
     <style>
-        *,
-        *:after,
-        *:before {
+        * {
             box-sizing: border-box;
         }
 
@@ -29,6 +28,7 @@
             color: var(--c-text-primary);
             background-color: var(--c-background-primary);
             line-height: 1.5;
+            padding: 10px;
         }
 
         input,
@@ -104,7 +104,7 @@
             border: none;
             background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23ccc' stroke-width='3' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
             background-color: transparent;
-            padding: 3rem;
+            padding: 2rem;
             width: 100%;
             display: flex;
             flex-direction: column;
@@ -122,26 +122,18 @@
             height: 2.25rem;
         }
 
-        .upload-area-icon svg {
-            max-height: 100%;
-            max-width: 100%;
-        }
-
         .upload-area-title {
             margin-top: 1rem;
             display: block;
             font-weight: 700;
             color: var(--c-text-primary);
+            text-align: center;
         }
 
         .upload-area-description {
             display: block;
             color: var(--c-text-secondary);
-        }
-
-        .upload-area-description strong {
-            color: var(--c-action-primary);
-            font-weight: 700;
+            text-align: center;
         }
 
         .modal-footer {
@@ -188,38 +180,25 @@
         #images {
             display: none;
         }
+
+        @media (max-width: 600px) {
+            .upload-area {
+                padding: 1rem;
+            }
+
+            .modal-header {
+                padding: 1rem;
+            }
+
+            .modal-body {
+                padding: 1rem;
+            }
+        }
     </style>
 </head>
 
-<body>
+<body style="padding-bottom: 0px;margin-bottom: 0px;background-color: #1e6c93;">
     <div class="modal">
-        <div class="modal-header">
-            <div class="modal-logo">
-                <span class="logo-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="512"
-                        height="419.116" viewBox="0 0 512 419.116">
-                        <defs>
-                            <clipPath id="clip-folder-new">
-                                <rect width="512" height="419.116" />
-                            </clipPath>
-                        </defs>
-                        <g id="folder-new" clip-path="url(#clip-folder-new)">
-                            <path id="Union_1" data-name="Union 1"
-                                d="M16.991,419.116A16.989,16.989,0,0,1,0,402.125V16.991A16.989,16.989,0,0,1,16.991,0H146.124a17,17,0,0,1,10.342,3.513L227.217,57.77H437.805A16.989,16.989,0,0,1,454.8,74.761v53.244h40.213A16.992,16.992,0,0,1,511.6,148.657L454.966,405.222a17,17,0,0,1-16.6,13.332H410.053v.562ZM63.06,384.573H424.722L473.86,161.988H112.2Z"
-                                fill="var(--c-action-primary)" />
-                        </g>
-                    </svg>
-                </span>
-            </div>
-            <button class="btn-close">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path fill="none" d="M0 0h24v24H0V0z" />
-                    <path
-                        d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"
-                        fill="var(--c-text-secondary)" />
-                </svg>
-            </button>
-        </div>
         <div class="modal-body">
             <h2 class="modal-title">Upload a file</h2>
             <p class="modal-description">Attach the file below</p>
@@ -233,11 +212,7 @@
                         <span class="upload-area-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="340.531" height="419.116"
                                 viewBox="0 0 340.531 419.116">
-                                <g id="files-new" clip-path="url(#clip-files-new)">
-                                    <path id="Union_2" data-name="Union 2"
-                                        d="M-2904.708-8.885A39.292,39.292,0,0,1-2944-48.177V-388.708A39.292,39.292,0,0,1-2904.708-428h209.558a13.1,13.1,0,0,1,9.3,3.8l78.584,78.584a13.1,13.1,0,0,1,3.8,9.3V-48.177a39.292,39.292,0,0,1-39.292,39.292Zm-13.1-379.823V-48.177a13.1,13.1,0,0,0,13.1,13.1h261.947a13.1,13.1,0,0,0,13.1-13.1V-323.221h-52.39a26.2,26.2,0,0,1-26.194-26.195v-52.39h-196.46A13.1,13.1,0,0,0-2917.805-388.708Zm146.5,241.621a14.269,14.269,0,0,1-7.883-12.758v-19.113h-68.841c-7.869,0-7.87-47.619,0-47.619h68.842v-18.8a14.271,14.271,0,0,1,7.882-12.758,14.239,14.239,0,0,1,14.925,1.354l57.019,42.764c.242.185.328.485.555.671a13.9,13.9,0,0,1,2.751,3.292,14.57,14.57,0,0,1,.984,1.454,14.114,14.114,0,0,1,1.411,5.987,14.006,14.006,0,0,1-1.411,5.973,14.653,14.653,0,0,1-.984,1.468,13.9,13.9,0,0,1-2.751,3.293c-.228.2-.313.485-.555.671l-57.019,42.764a14.26,14.26,0,0,1-8.558,2.847A14.326,14.326,0,0,1-2771.3-147.087Z"
-                                        transform="translate(2944 428)" fill="var(--c-action-primary)" />
-                                </g>
+                                <!-- SVG Content -->
                             </svg>
                         </span>
                         <span class="upload-area-title">Drag file(s) here to upload.</span>
@@ -257,47 +232,6 @@
             </form>
         </div>
     </div>
-
-    <script>
-        $(document).ready(function() {
-            $('#cancelButton').on('click', function() {
-                window.location.href = '{{ url('/') }}';
-            });
-
-            $('#uploadForm').on('submit', function(e) {
-                e.preventDefault();
-
-                const formData = new FormData(this);
-
-                $.ajax({
-                    url: $(this).attr('action'),
-                    type: 'POST',
-                    data: formData,
-                    contentType: false,
-                    processData: false,
-                    success: function(response) {
-                        Swal.fire({
-                            title: 'Success!',
-                            text: 'Your file has been uploaded successfully.',
-                            icon: 'success',
-                            confirmButtonText: 'OK'
-                        }).then(() => {
-                            window.location.href =
-                                '{{ url('/') }}';
-                        });
-                    },
-                    error: function(jqXHR, textStatus, errorThrown) {
-                        Swal.fire({
-                            title: 'Error!',
-                            text: 'An error occurred while uploading the file.',
-                            icon: 'error',
-                            confirmButtonText: 'OK'
-                        });
-                    }
-                });
-            });
-        });
-    </script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
