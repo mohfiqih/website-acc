ffb3d7ebfe74b5183b6eaaf0c357880b506ec5c7
mohfiqiherinsyah <fiqiherinsyah@bangunindo.com>
2025-02-09 09:35:01 +0700

    update upload images landing page acc

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -13,26 +13,6 @@ class ImageUploadController extends Controller
         return view('upload.index');
     }
 
-    // public function store(Request $request)
-    // {
-    //     $request->validate([
-    //         'images.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:10240'
-    //     ]);
-
-    //     if ($request->hasfile('images')) {
-    //         foreach ($request->file('images') as $image) {
-    //             $filename = time() . '_' . $image->getClientOriginalName();
-    //             $destinationPath = '/home/amaz9546/public_html/upload_image';
-    //             $image->move($destinationPath, $filename);
-    //             Image::create([
-    //                 'filepath' => 'upload_image/' . $filename,
-    //             ]);
-    //         }
-    //     }
-
-    //     return back()->with('success', 'Images uploaded successfully');
-    // }
-
     public function store(Request $request)
     {
         $request->validate([
